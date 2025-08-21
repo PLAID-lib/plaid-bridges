084fc4f3d5ad6fc3f2433286f66df8b4eed0b2be
Fabien Casenave <fabien.casenave@gmail.com>
2025-08-21 19:30:55 +0200

    fix for python 3.9

--- src/plaid_bridges/torch/grid.py
+++ src/plaid_bridges/torch/grid.py
@@ -25,33 +25,33 @@ class GridFieldsAndScalarsDataset(BaseRegressionDataset):
             dataset, in_feature_identifiers, out_feature_identifiers, online_transform
         )
 
-        dims = tuple(dimensions)
+        self.dims = tuple(dimensions)
 
-        @staticmethod
-        def _transform_sample(feature, feature_ids):
-            _type = feature_ids["type"]
-            if _type == "scalar":
-                treated_feature = feature
-            elif _type == "field":
-                treated_feature = feature.reshape(dims)
-            else:
-                raise Exception(
-                    f"feature type {_type} not compatible with `GridFieldsAndScalarsTransformer`"
-                )  # pragma: no cover
-            return torch.tensor(treated_feature)
-
-        @staticmethod
-        def _create_tensor(features, feature_identifiers):
-            tensor = torch.empty((len(features), len(feature_identifiers), *dims))
-            for i, feature in enumerate(features):
-                for j, feat_id in enumerate(feature_identifiers):
-                    tensor[i, j, ...] = _transform_sample(feature[j], feat_id)
-            return tensor
-
-        self.in_features = _create_tensor(self.in_features, self.in_feature_identifiers)
-        self.out_features = _create_tensor(
+        self.in_features = self._create_tensor(self.in_features, self.in_feature_identifiers)
+        self.out_features = self._create_tensor(
             self.out_features, self.out_feature_identifiers
         )
+
+    def _transform_sample(self, feature, feature_ids):
+        _type = feature_ids["type"]
+        if _type == "scalar":
+            treated_feature = feature
+        elif _type == "field":
+            treated_feature = feature.reshape(self.dims)
+        else:
+            raise Exception(
+                f"feature type {_type} not compatible with `GridFieldsAndScalarsTransformer`"
+            )  # pragma: no cover
+        return torch.tensor(treated_feature)
+
+
+    def _create_tensor(self, features, feature_identifiers):
+        tensor = torch.empty((len(features), len(feature_identifiers), *self.dims))
+        for i, feature in enumerate(features):
+            for j, feat_id in enumerate(feature_identifiers):
+                tensor[i, j, ...] = self._transform_sample(feature[j], feat_id)
+        return tensor
+
 
     @staticmethod
     def inverse_transform_single_feature(feat_id, predicted_feature):
